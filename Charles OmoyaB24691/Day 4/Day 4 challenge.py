a1ec1cd56456f207528ebd24825995cd57860929
The-Alpha-256 <144892600+The-Alpha-256@users.noreply.github.com>
2023-09-24 14:46:51 +0300

    day 4

---
+++ Charles OmoyaB24691/Day 4/Day 4 challenge.py
@@ -0,0 +1,10 @@
+StudentName=("Alice")
+grade1=85
+grade2=90
+grade3=88
+average_grade=(grade1 + grade2 + grade3)/3
+print(f"Student Name:{StudentName}")
+print(f"Grade 1:{grade1}")
+print(f"Grade 2:{grade2}")
+print(f"Grade 3:{grade3}")
+print(f"Average Grade:{average_grade}")
