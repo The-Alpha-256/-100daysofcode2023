439817a4ab0a8a194b32ec2effe1c0b679b4c0e0
The-Alpha-256 <144892600+The-Alpha-256@users.noreply.github.com>
2023-09-24 14:08:38 +0300

    day 3 update

--- Charles OmoyaB24691/Day 3/day 3.py
+++ Charles OmoyaB24691/Day 3/day 3.py
@@ -2,4 +2,11 @@ def summation():
         x=45*3/2+20\
         -10
         print(x)
-print("This is a statement outside the function")        
+print("This is a statement outside the function")
+summation()
+def multiply():
+        z=(200*50
+           *2
+           *34)
+        print(z)
+multiply()
